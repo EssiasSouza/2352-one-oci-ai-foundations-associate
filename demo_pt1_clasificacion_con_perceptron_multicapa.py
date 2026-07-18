03f13263e01f947cba860e2a77d5e22f7de3e992
Essias Souza <doutorska@gmail.com>
2026-07-18 15:47:42 -0300

    Add comments to organize code blocks

--- demo_pt1_clasificacion_con_perceptron_multicapa.py
+++ demo_pt1_clasificacion_con_perceptron_multicapa.py
@@ -1,12 +1,12 @@
-# Importar las librerías
+# Bloc [1] Importar las librerías
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_circles
 
-# Generar datos sinteticos (circulos)
+# Bloc [2] Generar datos sinteticos (circulos)
 x, y = make_circles(n_samples=300, noise=0.1, factor=0.5, random_state=0)
 
-# Representa los puntos con sus etiquetas.
+# Bloc [3] Representa los puntos con sus etiquetas.
 plt.figure(figsize=(8,6))
 plt.scatter(x[:,0], x[:,1], c=y, cmap=plt.cm.RdYlGn, edgecolors='k', marker='o', s=50)
 plt.xlabel('Característica 1 (x[:,0])')
