243b13485b0c964701e677ef2aa5f39d8e9edbe8
Essias Souza <doutorska@gmail.com>
2026-07-18 14:46:17 -0300

    Add a simple addition script in Python

---
+++ demo_intro_al_jupyter_notebooks.py
@@ -0,0 +1,9 @@
+# Esta aplicación agrega dos números.
+numero1 = 1
+numero2 = 4
+
+# Suma de los números
+suma = numero1 + numero2
+
+# Muestra el resultado
+print(f"La suma de {numero1} y {numero2} es: {suma}")
